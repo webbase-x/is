526c0a58f6acf8b585f7b6574aa5712a53340cd5
webbase-x <teacher.phairat@gmail.com>
2026-07-20 21:38:06 +0700

    แยกบทเรียนเดิมไว้ใน P2 และสร้างหน้าเว็บหลักใหม่

--- P2/js/config.js
+++ P2/js/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  appName: "เพลินภาษา",
+  appName: "คำไทยผจญภัย",
   projectId: "xnpzkhjodokvcgzovlxx",
   supabaseUrl: "https://xnpzkhjodokvcgzovlxx.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhucHpraGpvZG9rdmNnem92bHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxNjQ0OTMsImV4cCI6MjA5OTc0MDQ5M30.EXptzFubApuuUahH1mZCwx6Mw0UP6CJO00sB-kQhlu8",
